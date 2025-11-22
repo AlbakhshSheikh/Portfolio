18014a5d6b41cb3c23a74ede7b3e0a2beeb9bcd1
AlbakhshSheikh <albakhshsheikhofficial@gmail.com>
2025-11-23 00:09:52 +0530

    navbarupdate

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="pb-20">
       <Navbar />
       <Home />
       <About />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="fixed bottom-0 w-full text-center p-4 bg-transparent text-gray-400 border-t border-gray-700 glass z-40">
-            <p className="text-sm">
+        <div className="fixed bottom-0 w-full text-center p-2 md:p-4 bg-primary/90 text-gray-400 border-t border-gray-700 glass z-40 backdrop-blur-md">
+            <p className="text-xs md:text-sm">
                 Made With <span className="text-red-500">❤️</span> by Albakhsh Sheikh
             </p>
         </div>
